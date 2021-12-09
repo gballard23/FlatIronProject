890b7d85fef7771a978aa1634bec6dd67687993b
Gerren <g.ballard23@outlook.com>
2021-12-09 14:22:25 -0600

    update

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
 const name = ["Captian America: The First Avenger", "Captian Marvel", "Iron Man", "Iron Man 2","Thor","Avengers","Thor: The Dark World","Iron Man 3","Captian America: The Winter Soldier","Guardians of the Galaxy","Guardians of the Galaxy vol.2","Avengers: Age of Ultron","Ant-Man","Captain America: Civil War","Black Panther","Black Widow","Doctor Strange","Thor Ragnarok","Ant-Man and the Wasp","Avengers: Infinity War","Avengers: Endgame","Spider-Man: Homecoming","Spider-Man: Far From Home","Loki","WandaVision","The Falcon and the Winter Soldier","Shang-chi and the Legend of the Ten Rings","Hawkey"]
 
+const name2 = name.map(name =>name.toUpperCase());
 function index(array){
-  return name.indexOf(array) +1;
+  return name2.indexOf(array) +1;
 }
 
-let container = document.getElementsByClassName('.movie');
+let container = document.querySelector('.movie');
 
 const init = async () => {
   const inputForm = document.querySelector('form');
@@ -15,7 +16,7 @@ const init = async () => {
 
     console.log(input.value);
 
-    let id = index(input.value);
+    let id = index(input.value.toUpperCase());
 
     let url = `http://localhost:3000/mcu/${id}`;
 
@@ -23,20 +24,23 @@ const init = async () => {
    const data = await res.json(); 
     
       console.log(data);
+
+     
+      
       let temp = '';
-   
-      data.forEach(match => {
-        temp += `
-          <div class="posts">
-            <h2><strong>${match.title}</strong></h2>
-            <p>Released:${match.released}</p>
-            <p>Director:${match.director}</p>
-            <p>Producer:${match.producer}</p>
-            <p>Starring:${match.starring}</p>
-          </div>
-        `
     
-    })
+      
+
+        console.log(temp += `
+          <div class="posts">
+            <h2 id="title"><strong>${data.title}</strong></h2>
+            <p id="date">Released:${data.released}</p>
+            <p id='direct'>Director:${data.director}</p>
+            <p id='producer'>Producer:${data.producer}</p>
+            <p id='star'>Starring:${data.starring}</p>
+          </div>
+        `)
+    
 
     container.innerHTML = temp;
 
@@ -46,24 +50,5 @@ const init = async () => {
 
 window.addEventListener('DOMContentLoaded', init);
 
-   /*let temp = '';
-    posts.forEach(post => {
-        temp += `
-          <div class="posts">
-            <h2><strong>${post.title}</strong></h2>
-            <p>Released:${post.released}</p>
-            <p>Director:${post.director}</p>
-            <p>Producer:${post.producer}</p>
-            <p>Starring:${post.starring}</p>
-          </div>
-        `
-    
-    })
 
-container.innerHTML = temp;
-
-}
-
-
-window.addEventListener('DOMContentLoaded', () => renderData());*/
 
