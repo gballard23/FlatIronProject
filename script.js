4bf638d303dbdb5714ad3aa4d002ada45c5c7e36
Gerren <g.ballard23@outlook.com>
2021-12-15 14:43:10 -0600

    Completed Assignment

--- script.js
+++ script.js
@@ -1,29 +1,29 @@
 const name = ["Captain America: The First Avenger", "Captain Marvel", "Iron Man", "Iron Man 2","Thor","Avengers","Thor: The Dark World","Iron Man 3","Captain America: The Winter Soldier","Guardians of the Galaxy","Guardians of the Galaxy vol.2","Avengers: Age of Ultron","Ant-Man","Captain America: Civil War","Black Panther","Black Widow","Doctor Strange","Thor Ragnarok","Ant-Man and the Wasp","Avengers: Infinity War","Avengers: Endgame","Spider-Man: Homecoming","Spider-Man: Far From Home","Loki","WandaVision","The Falcon and the Winter Soldier","Shang-chi and the Legend of the Ten Rings","Hawkeye"]
-//The above array is made for the function below to return the index number plus one that also matches the ID of the resources in the json server
+
 const name2 = name.map(name =>name.toUpperCase());
 function index(array){
   return name2.indexOf(array) +1;
 }
-//Name2 captalizes the name variable before it is used in the index function
+
 let container = document.querySelector('.movie');
-//The container variable is created to hold the div in which we inject the fetch data from json
+
 const init = async () => {
   const inputForm = document.querySelector('form');
-//This function and input watch the html form for user submit      
+     
   inputForm.addEventListener('submit', async (event) => {
     event.preventDefault();
     const input = document.querySelector('input#search');
-//The submit reads the users input from input form in the html 
+
     console.log(input.value);
 
     let id = index(input.value.toUpperCase());
-//The ID variable is set to equal the users input and in all caps using the index function to match it with the array index which number also matches the ID of resources in th server 
+
     let url = `http://localhost:3000/mcu/${id}`;
-//The url varible contains the path and the id is decided by the users input that is sent through the index variable. 
+
    const res = await fetch(url);
    const data = await res.json(); 
-//The res in the fetch that grabs the data that the url data holds then passes that data to the data variable
-//The data variable then holds that resulting data in a javascript object format
+   
+
       console.log(data);
 
     let newElement = document.createElement('div');
@@ -38,8 +38,7 @@ const init = async () => {
     
       
       let temp = '';
-    //The temp varible is set to an empty string to hold all the values collected from the json server
-      
+   
 
         temp = `
           <div class="posts">
@@ -55,13 +54,11 @@ const init = async () => {
 
 
    
-//The container takes the temp varible data to be displayed in the html file
     
   })
 }
 
 window.addEventListener('DOMContentLoaded', init);
-//This event listener listens for the DOM and the init function that contains all of the functionality
 
 const form = document.querySelector('form');
 form.addEventListener('submit', myPlay);
